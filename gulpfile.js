103ac3e4fa86aa4229fe204e1e4d3c4b9b2cff05
Masoom <masoomulhaqs@gmail.com>
2016-07-26 16:23:10 +0530

    NEW LOOK #2

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 (function() {
-    var gulp = require('gulp');
-    var browserSync = require('browser-sync').create();
-    var reload      = browserSync.reload;
+    var gulp = require('gulp'),
+
+    compass = require('gulp-compass'),
+    cleanCSS = require('gulp-clean-css'),
+    cssShorthand = require('gulp-shorthand'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    dedupe = require('gulp-dedupe'),
+
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload;
 
     // Static server
     gulp.task('server', function() {
@@ -10,10 +18,25 @@
                 baseDir: "./"
             }
         });
+        gulp.watch("assets/scss/**/*.scss", ['compass']);
         gulp.watch("*.html").on("change", reload);
     });
 
     // SCSS Watcher & Compiler
+    gulp.task('compass', function() {
+        return gulp.src('assets/scss/**/*.scss')
+            .pipe(compass({
+                css: 'assets/css',
+                sass: 'assets/scss',
+                require: ['compass', 'breakpoint', 'singularitygs'],
+                style: 'expanded'
+            }))
+            .pipe(gulp.dest('assets/css'))
+            .pipe(gulp.dest('src'))
+            .pipe(concat('mk-spinners.min.css'))
+            .pipe(cleanCSS())
+            .pipe(gulp.dest('src'));
+    });
 
-    gulp.task('default', ['server']);
+    gulp.task('default', ['server', 'compass']);
 })();
